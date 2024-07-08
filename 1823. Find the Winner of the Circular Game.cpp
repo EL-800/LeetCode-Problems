05413d8dacc7fe35c11cd87add09ec14aaf06147
EL-800 <asesino1564@gmail.com>
2024-07-08 12:04:24 -0600

    1823

---
+++ 1823. Find the Winner of the Circular Game.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        if (n == 1)
+            return 1;
+        return ((findTheWinner(n - 1,k) + k - 1) % n) + 1;
+    }
+};
+
+int main() {
+    return 0;
+}
