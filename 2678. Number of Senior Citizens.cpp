a57f16616ac1380911f93f2e254ed9906541e806
EL-800 <asesino1564@gmail.com>
2024-08-01 11:05:02 -0600

    2678

---
+++ 2678. Number of Senior Citizens.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int ans = 0;
+        for (int i = 0; i < details.size(); i++) {
+            int age = (details[i][11] - '0') * 10 + (details[i][12] - '0');
+            ans += age > 60;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
