bb4af472ddafbbd370865ea7ba6d4d78a16334f1
EL-800 <asesino1564@gmail.com>
2024-08-28 17:48:54 -0600

    1905

--- 1905. Count Sub Islands.cpp
+++ 1905. Count Sub Islands.cpp
@@ -8,13 +8,11 @@ private:
 
     bool DFS(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int row, int col) {
         if (row < 0 || row >= grid1.size() || col < 0 || col >= grid1[0].size())
-            return true;
+            return true;        
         if (!grid2[row][col])
             return true;
-        grid2[row][col] = false;
-        if (!grid1[row][col])
-            return false;        
-        bool ans = true;
+        grid2[row][col] = false;            
+        bool ans = grid1[row][col];
         for (auto [mR, mC] : moves)
             ans &= DFS(grid1, grid2, row + mR, col + mC);
         return ans;
@@ -24,8 +22,10 @@ public:
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
         int ans = 0;
         for (int i = 0; i < grid1.size(); i++) {
-            for (int j = 0; j < grid1[0].size(); j++)
-                ans += DFS(grid1, grid2, i, j);
+            for (int j = 0; j < grid1[0].size(); j++) {
+                if (grid2[i][j])
+                    ans += DFS(grid1, grid2, i, j);
+            }
         }
         return ans;
     }
