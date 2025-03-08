34e19d8e77177d71982f2a6336c2fe930ff4fc2d
EL-800 <asesino1564@gmail.com>
2025-03-08 12:06:12 -0600

    2379

---
+++ 2379. Minimum Recolors to Get K Consecutive Black Blocks.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    public:
+        int minimumRecolors(string blocks, int k) {
+            int n = blocks.length(), left = 0, right = 0, countW = 0;
+            while (right < k) {
+                countW += blocks[right] == 'W';
+                right++;
+            }
+            int ans = countW;
+            while (right < n) {
+                countW -= blocks[left] == 'W';
+                countW += blocks[right] == 'W';
+                ans = min(ans, countW);
+                left++;
+                right++;
+            }
+            return countW;                 
+        }
+    };
+
+int main() {
+    Solution s;
+    s.minimumRecolors("WBBWWBBWBW", 7);
+    return 0;
+}
