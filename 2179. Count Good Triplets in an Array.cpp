b5655ecece90bfc5a5be1b8331d5c9f94d6822e8
EL-800 <asesino1564@gmail.com>
2025-04-15 12:12:37 -0600

    2179

--- 2179. Count Good Triplets in an Array.cpp
+++ 2179. Count Good Triplets in an Array.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 
 using namespace std;
+using namespace __gnu_pbds;
 
 class Solution {
     public:
@@ -8,11 +10,11 @@ class Solution {
             unordered_map<int, int> nums1Indexes;            
             for (int i = 0; i < nums1.size(); i++) 
                 nums1Indexes[nums1[i]] = i;            
-            set<int> seenIndexes;
+            tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> seenIndexes;
             long long total = 0 , n = nums1.size();            
             for (int i = 0; i < n; i++) {
                 int ind = nums1Indexes[nums2[i]];    
-                int left = distance(seenIndexes.begin(), seenIndexes.lower_bound(ind));
+                int left = seenIndexes.order_of_key(ind);
                 int right = (n - 1 - ind) - (seenIndexes.size() - left);
                 total += (long long) left * right;
                 seenIndexes.insert(ind);
