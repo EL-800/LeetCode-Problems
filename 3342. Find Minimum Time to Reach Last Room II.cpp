3ced59b97299a6c6344c7f7b62607506afc3f8d0
EL-800 <asesino1564@gmail.com>
2025-05-08 13:20:36 -0600

    3342

---
+++ 3342. Find Minimum Time to Reach Last Room II.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+    public:
+        int minTimeToReach(vector<vector<int>>& moveTime) {
+            int n = moveTime.size(), m = moveTime[0].size();
+            vector<pair<int, int>> moves = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+            priority_queue<vector<int>, vector<vector<int>>, greater<>> pendingMoves;
+            pendingMoves.push({0, 0, 0, 1});
+            moveTime[0][0] = -1;
+            while (!pendingMoves.empty()) {
+                int row = pendingMoves.top()[1], col = pendingMoves.top()[2], val = pendingMoves.top()[0], alternateTime = pendingMoves.top()[3];
+                pendingMoves.pop();
+                if (row == n - 1 && col == m -1)
+                    return val;
+                for (auto [moveR, moveC] : moves) {
+                    int adjR = row + moveR, adjC = col + moveC;
+                    if (adjR >= n || adjC >= m || adjR < 0 || adjC < 0)
+                        continue;
+                    if (moveTime[adjR][adjC] == -1)
+                        continue;
+                    pendingMoves.push({max(moveTime[adjR][adjC], val) + alternateTime, adjR, adjC, alternateTime == 1 ? 2 : 1});  
+                    moveTime[adjR][adjC] = -1;                                      
+                }
+            }
+            return -1;
+        }
+    };
+
+int main() {
+    return 0;
+}
