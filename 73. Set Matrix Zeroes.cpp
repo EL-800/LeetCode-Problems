a889c5485c65d7922279354f891a553977818b42
EL-800 <asesino1564@gmail.com>
2025-05-21 13:07:57 -0600

    73

---
+++ 73. Set Matrix Zeroes.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!matrix[i][j]) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {            
+            for (int j = m - 1; j >= 0; j--) {
+                if (!matrix[i][0] || !matrix[0][j])
+                    matrix[i][j] = 0;
+            }                
+        }        
+    }
+};
+
+int main() {
+    return 0;
+}
