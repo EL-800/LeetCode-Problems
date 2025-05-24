788ddadc7c5bc2c99295ec14be3a7a42652702d8
EL-800 <asesino1564@gmail.com>
2025-05-24 11:39:45 -0600

    2942

---
+++ 2942. Find Words Containing Character.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {        
+        vector<int> ans;
+        for (int i = 0; i < words.size(); i++) {
+            if (count(words[i].begin(), words[i].end(), x))
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
