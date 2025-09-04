d858691c18a86d83381f10e49d50b0dfe639c348
EL-800 <asesino1564@gmail.com>
2025-09-04 13:36:34 -0600

    3516

---
+++ 3516. Find Closest Person.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        if (abs(x - z) < abs(y - z))
+            return 1;
+        if (abs(x - z) > abs(y - z))
+            return 2;
+        return 0;
+    }
+};
+
+int main() {
+    return 0;
+}
