774a6e19d3a9153418f6644da54258f8771213ef
EL-800 <asesino1564@gmail.com>
2025-11-10 14:24:50 -0600

    3542

---
+++ 3542. Minimum Operations to Convert All Elements to Zero.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        stack<int> st;
+        int ans = 0;
+        for (int num : nums){
+            while(!st.empty() && num < st.top()) 
+                st.pop();
+            if (st.empty() || num > st.top()){
+                if (num > 0) 
+                    ans++;
+                st.push(num);
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
