f9b5367744047b3d95b249a6a3cbade6acd588a0
EL-800 <asesino1564@gmail.com>
2025-11-18 10:44:24 -0600

    717

---
+++ 717. 1-bit and 2-bit Characters.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int n = bits.size();
+        for (int i = 0; i < n; i++) {
+            if (i == n - 1)
+                return true;
+            if (bits[i])
+                i++;
+        }
+        return false;
+    }
+};
+
+int main() {
+    return 0;
+}
