7d9d2086f9626b136b8a8bfd47d86d59cf311dd9
EL-800 <asesino1564@gmail.com>
2025-11-29 10:21:50 -0600

    3512

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        return accumulate(nums.begin(), nums.end(), 0) % k;
+    }
+};
+
+int main() {
+    return 0;
+}
