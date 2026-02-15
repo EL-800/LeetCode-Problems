c33c8e511f40d9f9df55c907c1666bc50cbf3819
EL-800 <asesino1564@gmail.com>
2026-02-15 09:41:28 -0600

    67

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+        
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+            result = to_string(sum % 2) + result;
+            carry = sum / 2;
+        }
+        
+        return result;
+    }
+};
+
+
+int main() {
+    return 0;
+}
