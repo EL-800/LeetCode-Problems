0a4f4d2cb65106701f3ddcb264ef1c61fa850a50
EL-800 <asesino1564@gmail.com>
2026-06-17 11:15:33 -0600

    3614

---
+++ 3614. Process String with Special Operations II.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {    
+public:
+    char processStr(string s, long long k) {
+        int n = s.size();
+        vector<long long> sizes;
+        
+        long long currSize = 0;
+        for (auto c : s) {
+            if (c == '*')
+                currSize = max(currSize - 1, 0ll);
+            else if (c == '#')
+                currSize *= 2;
+            else if (c != '%')
+                currSize++;
+
+            sizes.push_back(currSize);
+        }
+
+        if (k >= currSize) 
+            return '.';
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '#' && k >= sizes[i] / 2)
+                k -= sizes[i] / 2;
+            else if (s[i] == '%')
+                k = sizes[i] - 1 - k;
+            else if (s[i] != '*' && sizes[i] == k + 1)
+                return s[i];
+            
+            /*
+            switch (s[i]) {
+            case '*':
+                break;
+            case '#':
+                if (k >= lens[i] / 2)
+                    k -= lens[i] / 2;
+                break;
+            case '%':
+                k = lens[i] - 1 - k;
+                break;
+            default: // s[i] is a character
+                if (lens[i] == k + 1)
+                    return s[i];
+            }*/
+        }
+
+        return '8';
+    }
+};
+
+int main() {
+    return 0;
+}
