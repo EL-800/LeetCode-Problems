a516f8b931e8122851a3aa540d77f9f8a65f54dc
EL-800 <asesino1564@gmail.com>
2026-06-29 11:25:20 -0600

    1967

---
+++ 1967. Number of Strings That Appear as Substrings in Word.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int ans = 0;
+        for (auto &p: patterns) 
+            ans += word.find(p) != string::npos;     
+            
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
