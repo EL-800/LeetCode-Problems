06b08a00b0304d7792870fa34a7cc94fc8f0e272
EL-800 <asesino1564@gmail.com>
2026-09-02 11:39:54 -0600

    3875

---
+++ 3875. Construct Uniform Parity Array I.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
+
+int main() {
+    return 0;
+}
