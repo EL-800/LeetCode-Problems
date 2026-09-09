a8382b00d0fb04ec953d568847c91c7bdd781d0d
EL-800 <asesino1564@gmail.com>
2026-09-09 10:10:32 -0600

    3871

---
+++ 3871. Count Commas in Range II.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        for (long long i = 1000; i <= n; i *= 1000) 
+            ans += n - i + 1;
+        return ans;
+    }
+};
+
+int main() {
+    return 0;
+}
